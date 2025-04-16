e60f874d4ad5f5c67c721f4e928c969c92ae65ec
Jesper Sandberg Hesselgren <jespersh@kth.se>
2025-04-16 17:55:09 +0200

    rebuiling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-
-import { Introduction } from "@/components/introduction"
-import { AboutMe } from "@/components/aboutMe"
-import { Projects2 } from "@/components/projects2"
-import { Exploring } from "@/components/exploring"
-import { Footer } from "@/components/footer"
 import { Presentation } from "@/components/presentation"
 
 
@@ -13,11 +7,6 @@ function App() {
   return (
     <>
       <Presentation />  
-      {/* <Introduction />
-      <AboutMe />
-      <Projects2 />
-      <Exploring />
-      <Footer /> */}
     </>
   )
 }
